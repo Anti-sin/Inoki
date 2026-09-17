5856f3995dbc2d5953222e864c9312deb166f1d6
Ankit <ankitbagde2003@gmail.com>
2026-09-17 21:45:19 +0530

    solved the 404

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getMenu = async () => {
-  const response = await fetch(`${API_URL}/api/menu`);
+  const response = await fetch(`${API_URL}api/menu`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch menu");
